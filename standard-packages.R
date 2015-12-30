9e01fd2306b32896cf2ffde0630e8f8ee33cc31b
MattAltieri <matt.onlystory@gmail.com>
2015-12-30 09:51:52 -0500

    Added the best list of candidate packages for testing.

---
+++ standard-packages.R
@@ -0,0 +1,34 @@
+p <- c("devtools",
+       "caret",
+       "data.table",
+       "doParallel",
+       "dplyr",
+       "e1071",
+       "forecase",
+       "foreign",
+       "ggplot2",
+       "ggthemes",
+       "Hmisc",
+       "knitr",
+       "lattice",
+       "lubridate",
+       "magrittr",
+       #"nlme",
+       "plyr",
+       "quanteda",
+       "quantedaData",
+       "reshape2",
+       "RODBC",
+       "RWeka",
+       "shiny",
+       #"stringi",
+       "stringr",
+       "testthat",
+       "tm",
+       "xtable"
+       )
+install.packages(p)
+
+library(devtools)
+install_github("ramnathv/slidify")
+install_github("ramnathv/slidifyLibraries")
